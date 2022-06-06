6319d3ad1d32aa935f03582cdec5605205462888
alemenew <alemenewendawek@gmail.com>
2022-06-07 02:30:33 +0300

    replaces an element of a list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list is not None:
-        i = len(my_list) - 1
-        while i >= 0:
-            print("{:d}".format(my_list[i]))
-            i -= 1
+def replace_in_list(my_list, idx, element):
+    """Replace an element of a list at a specific position."""
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
